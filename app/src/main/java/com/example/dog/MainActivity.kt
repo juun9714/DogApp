01b879090afe0c37023ee01d9fce5d61d55f61f7
june <lovekwon200@gmail.com>
2021-01-11 16:45:05 +0900

    connected story menu

--- app/src/main/java/com/example/dog/MainActivity.kt
+++ app/src/main/java/com/example/dog/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.dog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toast.makeText
+import androidx.core.view.GravityCompat
 import com.example.dog.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,6 +18,23 @@ class MainActivity : AppCompatActivity() {
         var view = binding.root
         setContentView(view)
 
+        binding.nvBtn.setOnClickListener {
+            binding.draw.openDrawer(GravityCompat.START)
+        }
+        binding.nvMenu.setNavigationItemSelectedListener(this)
+    }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.story)
+            startActivity(Intent(this,story::class.java))
+        binding.draw.closeDrawers()
+        return false
+    }
+
+    override fun onBackPressed() {
+        if(binding.draw.isDrawerOpen(GravityCompat.START))
+            binding.draw.closeDrawers()
+        else
+            super.onBackPressed()
     }
 }
---
+++ app/src/main/java/com/example/dog/story.kt
@@ -0,0 +1,42 @@
+package com.example.dog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import com.example.dog.databinding.ActivityMainBinding
+import com.example.dog.databinding.ActivityStoryBinding
+import com.google.android.material.navigation.NavigationView
+
+class story : AppCompatActivity(),  NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var binding : ActivityStoryBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStoryBinding.inflate(layoutInflater)
+        var view = binding.root
+        setContentView(view)
+
+        binding.nvBtnStory.setOnClickListener {
+            binding.drawStory.openDrawer(GravityCompat.START)
+        }
+        binding.nvMenuStory.setNavigationItemSelectedListener(this)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.home)
+            startActivity(Intent(this,MainActivity::class.java))
+        binding.drawStory.closeDrawers()
+        return false
+    }
+
+    override fun onBackPressed() {
+        if(binding.drawStory.isDrawerOpen(GravityCompat.START))
+            binding.drawStory.closeDrawers()
+        else
+            super.onBackPressed()
+    }
+
+
+}
